00f7248c5320634d8f0985e6c987112dd98da843
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-10-09 18:05:45 +0900

    socket.io room/chat 네임스페이스 구분/ 이벤트 추가중

--- socket.js
+++ socket.js
@@ -2,6 +2,23 @@ const SocketIO = require('socket.io');
 
 module.exports = (server) =>{
     const io = SocketIO(server,{ path: '/socket.io' });
+    /* 네임스페이스
+     네임스페이스로 실시간 데이터가 전달될 주소를 구분할 수 있다. 기본 네임스페이스는 / 이다.
+    기본값은 io.of('/') (io.on으로 생략했었음.)
+    방 안에 들어가지도 않았는데 방 안의 모든 데이터 다 받아올 필요 없으므로 네임스페이스 나눈다.
+    room 네임스페이스는 채팅 방 목록에 관한 실시간 이벤트만 받는 네임스페이스로, chat 네임스페이스는 채팅에 대한 이벤트만 받는 네임스페이스로 한다.
+    불필요한 실시간 정보가 전달 안되도록 하기 위함이다. (서버 쪽 자원과 프론트쪽 자원을 아끼기 위함.)
+    */
+
+    const room = io.of('/room');
+    const chat = io.of('/chat');
+
+    room.on('conncetion',(socket)=>{
+        console.log('room 네임스페이스에 접속.');
+        socket.on('disconnect',()=>{
+            console.log('room 네임스페이스 접속 해제');
+        });
+    });
 
     io.on('connection',(socket) => {
        const req = socket.request;
