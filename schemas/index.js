2e47b6f1ec5fbf44ca597c499c5f7c37352a7fc5
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-10-09 17:26:43 +0900

    mongoDB 접속/재접속 index.js

---
+++ schemas/index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const {MONGO_ID, MONGO_PASSWORD, NODE_ENV} = process.env;
+const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/admin`;  //dotenv파일 안에 아이디 패스워드 있음.
+//mongoDB는 접속 주소로 접속.
+
+module.exports = () =>{
+    const connect = () =>{
+        mongoose.connect = ()=>{
+            mongoose.connect(MONGO_URL,{
+                dbName:'gifchat',
+            },(error)=>{
+                if(error){
+                    console.log('몽고디비 연결 에러',error);
+                }else{
+                    console.log('몽고디비 연결 성공!');
+                }
+            });
+        };
+    };
+    connect();
+
+    mongoose.connection.on('error',(error)=>{
+        console.error('몽고디비 연결 에러',error);
+    });
+    mongoose.connection.on('disconnected',()=>{
+        console.error('몽고디비 연결이 끊어졌습니다. 연결을 재시도합니다.');
+        connect();
+    });
+    require('./chat');
+    require('./room');
+};
