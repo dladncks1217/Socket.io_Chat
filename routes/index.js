2bf518228a39ad68bdc5634c1efccbd5a246ae14
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-10-26 13:43:59 +0900

    (화면상) 채팅방 라우터 추가

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,10 @@ router.get('/',async (req,res, next)=>{
     }
 });
 
+router.get('/room',(req,res)=>{
+    res.render('room', {title:'GIF 채팅방 생성'});
+});  // (화면에 보여지는) 채팅방 라우터
+
 
 
 module.exports = router;
