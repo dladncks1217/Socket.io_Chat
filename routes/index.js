ee9e32246acd8332f24439133a599abd0e796729
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-10-26 13:40:55 +0900

    main.pug 렌더링

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
+
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('index');
+router.get('/',async (req,res, next)=>{
+    try{
+        const rooms = await Room.find({});
+        res.render('main',{
+            rooms,
+            title:'GIF 채팅방',
+            error: req.flash('roomError')});
+    } catch(error){
+        console.error(error);
+        next(error);
+    }
 });
+
+
 
 module.exports = router;
