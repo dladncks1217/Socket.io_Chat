7838501de1be1b8912d53647cbaec8429c91b4df
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-11-13 10:33:30 +0900

    DB 복구(현재시간 제거)

--- schemas/chat.js
+++ schemas/chat.js
@@ -4,16 +4,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { Types: {ObjectId}} = Schema;
 
-// 현재 시간 추가
-const date = new Date();
-let hour = date.getHours();
-let minutes = date.getMinutes();
-if(minutes<10){
-    minutes = `0${minutes}`;
-}
-if(hour<10){
-    hour = `0${hour}`;
-}
+
 const chatSchema = new Schema({
     room:{
         type: ObjectId,
@@ -30,10 +21,7 @@ const chatSchema = new Schema({
         type: Date,
         default: Date.now,
     },
-    chattime:{
-        type:String,
-        default: `${hour}:${minutes}`,
-    }
+
 });
 
 module.exports = mongoose.model('Chat', chatSchema);
