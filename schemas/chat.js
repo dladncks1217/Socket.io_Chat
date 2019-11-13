eacd62654b551c8d989bb4675b66c79f99e0da43
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-11-13 09:14:50 +0900

    DB chattime 추가

--- schemas/chat.js
+++ schemas/chat.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { Types: {ObjectId}} = Schema;
 
+// 현재 시간 추가
+const date = new Date();
+let hour = date.getHours();
+let minutes = date.getMinutes();
+if(minutes<10){
+    minutes = `0${minutes}`;
+}
+if(hour<10){
+    hour = `0${hour}`;
+}
 const chatSchema = new Schema({
     room:{
         type: ObjectId,
@@ -20,6 +30,10 @@ const chatSchema = new Schema({
         type: Date,
         default: Date.now,
     },
+    chattime:{
+        type:String,
+        default: `${hour}:${minutes}`,
+    }
 });
 
 module.exports = mongoose.model('Chat', chatSchema);
