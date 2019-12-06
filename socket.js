1a62ebca6af0c76f14c0fbb72810d24a91eee61d
dladncks1217 <dlaxodud1217@gmail.com>
2019-12-06 18:55:29 +0900

    socket.on dm 추가

--- socket.js
+++ socket.js
@@ -83,6 +83,9 @@ module.exports = (server, app, sessionMiddleware) =>{
              })
             }
         });
+        socket.on('dm',(data)=>{
+            socket.to(data.target).emit('dm',data);
+        });
     });
 };
 
