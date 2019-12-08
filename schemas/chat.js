52a76a7ca5045d43c0ca034c6903ce987199a841
dladncks1217 <dlaxodud1217@gmail.com>
2019-12-08 17:08:55 +0900

    MongoDB createdAt moment-timezone 제거

--- schemas/chat.js
+++ schemas/chat.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { Types: {ObjectId}} = Schema;
 
-
 const chatSchema = new Schema({
     room:{
         type: ObjectId,
@@ -18,8 +17,7 @@ const chatSchema = new Schema({
     chat:String,
     gif:String,
     createdAt:{
-        type: Date,
-        default: Date.now,
+        type: String,
     },
 
 });
